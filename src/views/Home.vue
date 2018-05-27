5a81b920a7269d7665ed8d76b1a80b0a8820cff2
Luiz Tanure <letanure@gmail.com>
2018-05-27 19:40:36 +0200

    build: Add pug as HTML preprocessor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
-<template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .home
+    img(src='../assets/logo.png',  )
+    HelloWorld(msg='Welcome to Your Vue.js App', )
 </template>
 
 <script>
